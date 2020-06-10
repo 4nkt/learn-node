9e67f8042565a59f35fdf9236e4e92572bd5df62
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-06-10 23:55:25 +0530

    Add some basic routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
   res.send('Hey! It works!');
 });
 
+router.get('/json', (req, res) => {
+  const data = { name: 'John', age: 30, cool: true };
+  res.json(data);
+});
+
+router.get('/query', (req, res) => {
+  res.json(req.query);
+});
+
+router.get('/reverse/:text', (req, res) => {
+  const reverse = [...req.params.text].reverse().join('');
+  res.send(reverse);
+});
+
 module.exports = router;
