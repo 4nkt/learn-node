f5dc15588da7d7cf4cc6694650298823e0a8c610
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-09-10 23:56:08 +0530

    Add login/logout endpoints and protected routes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post('/add',
   storeController.upload,
@@ -28,6 +28,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -42,5 +43,7 @@ router.post('/register',
   catchErrors(userController.register),
   authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
